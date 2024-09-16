15969a7a6250f998a1d9f574344910811c20fb22
Duy <duy.leduc8@gmail.com>
2024-09-16 17:54:02 +0700

    parse data from scraper to ollama LLM

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scrape import (
     split_dom_content,
     extract_body_content
 )
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter the website URL")
@@ -19,15 +20,16 @@ if st.button("Scrape Site"):
         st.session_state.dom_content = cleaned_content
         with st.expander("View DOM Content"):
             st.text_area("DOM Content", cleaned_content, height=300)
-
         st.write("Scraping complete!")
-
-        if "dom_content" in st.session_state:
-            parse_description = st.text_area("Describe what you want to parse from the DOM")
-
-            if st.button("Parse Content"):
-                if parse_description:
-                    st.write("Parsing the content...")
-                    dom_chunks = split_dom_content(st.session_state.dom_content)
     else:
         st.error("Please enter a valid URL")
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse from the DOM")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content...")
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
